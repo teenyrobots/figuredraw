d93b36dc5dfd0f66e1f0e2ce13c2cd1fedd5ad37
Joyce <hellojoycerice@gmail.com>
2020-03-16 11:24:53 -0400

    starts laying out img changing code

---
+++ script.js
@@ -0,0 +1,11 @@
+// every 10 seconds, change the image
+
+function imgChange() {
+
+  console.log("we're fucking changing it y'all");
+
+  let figure = document.getElementById('figure');
+
+  figure.src = "images/wutang.png";
+
+}
