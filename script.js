fae67d4d403f21646211d8de66125279a00b83a0
Joyce <hellojoycerice@gmail.com>
2020-05-19 16:43:59 -0400

    lets users add files

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 // every 10 seconds, change the image
 
+let imgs = [];
+let figure = document.getElementById('figure');
+let counter = 0;
+
+function start(){
+  let myFiles = document.getElementById('input').files;
+  for (i = 0; i < myFiles.length; i++) {
+    console.log(myFiles[i].name);
+    imgs.push(myFiles[i]);
+  }
+}
+
 function imgChange() {
 
-  console.log("we're fucking changing it y'all");
+  let currentFig = imgs[counter];
+  console.log(imgs[counter]);
 
-  let figure = document.getElementById('figure');
+  figure.src = URL.createObjectURL(currentFig);
+  figure.onload = function() {
+        URL.revokeObjectURL(this.src);
+      }
 
-  figure.src = "images/wutang.png";
+  if (counter < imgs.length - 1) {
+    counter++;
+  } else {
+    counter = 0;
+  }
 
 }
+
+// function start()
+// get a list of files in the folder
+    /// use "the file API"?
